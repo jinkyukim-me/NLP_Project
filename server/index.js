12f067dc6b83617727e85724ce77ad128540be15
biniruu <biniruu@gmail.com>
2019-11-15 23:42:58 +0900

    서버 - db 통신 구현 중

--- server/index.js
+++ server/index.js
@@ -17,17 +17,54 @@ const db = knex({
 app.use(express.json());
 app.use(cors());
 
-app.post('/user', (req, res) => {
+app.post('/login', (req, res) => {
   const email = req.body.email;
   
-  db.raw(`INSERT INTO user (email, created_data_time) VALUES (${email}, $now())`)
+  db.raw(`INSERT INTO user (email, created_data_time) VALUES ('${email}', now())`)
   .then((response) => {
     res.status(200).end('OK');
   })
   .catch((error) => {
-    console.log(error);
-    console.log(`EMAIL: ${email}`);
+    console.error(error);
     res.status(500).end('FAILED');
+  });
+});
+
+app.post('/post', (req, res) => {
+  const paragraph = req.body.paragraph;
+  const affectivity = req.body.affectivity;
+  
+  db.raw(`INSERT INTO post (create_post_date, paragraph, created_data_time) VALUES (now(), '${paragraph}', now())`)
+  .then((response) => {
+    res.status(200).end('OK');
+  })
+  .catch((error) => {
+    console.error(error);
+    res.status.end('FAILED');
+  });
+  
+  db.raw(`INSERT INTO post (affectivity, created_data_time) VALUES ('${affectivity}', now())`)
+  .then((response) => {
+    res.status(200).end('OK');
+  })
+  .catch((error) => {
+    console.error(error);
+    res.status(500).end('OK');
+  });
+});
+
+app.post('/edit_post', (req, res) => {
+  const userId = req.body.user_id;
+  const postId = req.body.post_id;
+  const paragraph = req.body.paragraph;
+  
+  db.raw(`UPDATE post SET id='${userId}', post_id='${postId}', paragraph='${paragraph}', modified_data_time=now()`)
+  .then((response) => {
+    res.status(200).end('OK');
+  })
+  .catch((error) => {
+    console.error(error);
+    res.status.end('FAILED');
   });
 });
 
