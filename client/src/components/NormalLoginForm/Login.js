302be6551a00676d13554651113efd21bb6a47db
biniruu <biniruu@gmail.com>
2019-11-18 01:52:22 +0900

    로그인 화면 스타일 수정

--- client/src/components/NormalLoginForm/Login.js
+++ client/src/components/NormalLoginForm/Login.js
@@ -19,41 +19,32 @@ class NormalLoginForm extends React.Component {
   const { getFieldDecorator } = this.props.form;
   return (
     <>
-      <Layout>
-        <section className="Content-section-layout">
-          <Form onSubmit={this.handleSubmit} className="login-form">
-            <Form.Item>
-              {getFieldDecorator('mail', {
-                rules: [{ required: true, message: '이메일 주소를 입력해주세요!' }],
-              })(
-                <Input
-                  prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  placeholder="이메일"
-                />,
-              )}
-            </Form.Item>
-            <Form.Item>
-              {getFieldDecorator('password', {
-                rules: [{ required: true, message: '비밀번호를 입력해주세요.!' }],
-              })(
-                <Input
-                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  type="password"
-                  placeholder="비밀번호"
-                />,
-              )}
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit" className="login-form-button">
-                로그인
-              </Button>
-              <div className="">
-                <Link to="">회원가입</Link>
-                <Link to="">비밀번호 찾기</Link>
-              </div>
-            </Form.Item>
-          </Form>
-        </section>
+      <Layout className="one-login flex flex-center">
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <Form.Item className="one-input one-input-email">
+            {getFieldDecorator('mail', {
+              rules: [{ required: true, message: '이메일 주소를 입력해주세요!' }],
+            })(
+              <Input prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="이메일" />,
+            )}
+          </Form.Item>
+          <Form.Item className="one-input one-input-pw">
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: '비밀번호를 입력해주세요.!' }],
+            })(
+              <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="비밀번호" />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button" block>
+              로그인
+            </Button>
+            <div className="shortcut flex">
+              <Link to="">회원가입</Link>
+              <Link to="">비밀번호 찾기</Link>
+            </div>
+          </Form.Item>
+        </Form>
       </Layout>
     </>  
     )
