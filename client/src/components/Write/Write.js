5cecf98b030df7ad7a2ec788431af90eacc3f589
biniruu <biniruu@gmail.com>
2019-11-19 11:52:45 +0900

    글 저장 confirm 기능 추가

--- client/src/components/Write/Write.js
+++ client/src/components/Write/Write.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 // import './index.css';
-import { Input, Button } from 'antd';
+import { Input, Button, Modal } from 'antd';
 
 const { TextArea } = Input;
 
 class Write extends React.Component {
   state = {
     value: '',
+    visible: false,
+  };
+  
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
   };
 
   onChange = ({ target: { value } }) => {
@@ -20,7 +41,10 @@ class Write extends React.Component {
     return (
       <div className="one-post-write">
         <TextArea placeholder="...그래서 오늘은 어땠어?" className="one-textarea" />
-        <Button type="primary" className="btn btn-submit">저장</Button>
+        <Button type="primary" onClick={this.showModal} className="btn btn-submit">저장</Button>
+        <Modal title="Basic Modal" visible={this.state.visible} onOk={this.handleOk} onCancel={this.handleCancel} >
+          <p>저장하시겠습니까?</p>
+        </Modal>
       </div>
     )
   }
